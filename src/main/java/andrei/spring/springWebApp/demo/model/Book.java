f1b913e91f843f73f65f2167a38527b79f52ec6e
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-19 14:43:47 +0300

    adding toString, equals and hascode

--- src/main/java/andrei/spring/springWebApp/demo/model/Book.java
+++ src/main/java/andrei/spring/springWebApp/demo/model/Book.java
@@ -1,19 +1,49 @@
 package andrei.spring.springWebApp.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
 import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String pages;
+
+    @ManyToMany
+    @JoinTable(name = "author book"m joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = "author_id")
     private Set<Author> authors;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", pages='" + pages + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
