3967b406b313210a1e675459ff77afdc7f5d8ca7
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-19 13:51:46 +0300

    initial commit

---
+++ src/test/java/andrei/spring/springWebApp/demo/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package andrei.spring.springWebApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
